3905fe5e9cd597d7762c1545a21b20e4a507ddf0
addeen <61187259+addeen@users.noreply.github.com>
2020-12-14 21:36:24 +0000

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,251 @@
+var Audio01= $("#Norway_Audio")[0];
+$("#Norway")
+.mouseenter(function(){
+    Audio01.play();
+});
+
+var Audio02= $("#Sweden_Audio")[0];
+$("#Sweden")
+.mouseenter(function(){
+    Audio02.play();
+});
+
+var Audio03= $("#Finland_Audio")[0];
+$("#Finland")
+.mouseenter(function(){
+    Audio03.play();
+});
+
+var Audio04= $("#Estonia_Audio")[0];
+$("#Estonia")
+.mouseenter(function(){
+    Audio04.play();
+});
+
+var Audio05= $("#Latvia_Audio")[0];
+$("#Latvia")
+.mouseenter(function(){
+    Audio05.play();
+});
+
+var Audio06= $("#Lithuania_Audio")[0];
+$("#Lithuania")
+.mouseenter(function(){
+    Audio06.play();
+});
+
+var Audio07= $("#Belarus_Audio")[0];
+$("#Belarus")
+.mouseenter(function(){
+    Audio07.play();
+});
+
+var Audio08= $("#Ukraine_Audio")[0];
+$("#Ukraine")
+.mouseenter(function(){
+    Audio08.play();
+});
+
+var Audio09= $("#Russia_Audio")[0];
+$("#Russia")
+.mouseenter(function(){
+    Audio09.play();
+});
+
+var Audio10= $("#Turkey_Audio")[0];
+$("#Turkey")
+.mouseenter(function(){
+    Audio10.play();
+});
+
+var Audio11= $("#Greece_Audio")[0];
+$("#Greece")
+.mouseenter(function(){
+    Audio11.play();
+});
+
+var Audio12= $("#Moldova_Audio")[0];
+$("#Moldova")
+.mouseenter(function(){
+    Audio12.play();
+});
+
+var Audio13= $("#Romania_Audio")[0];
+$("#Romania")
+.mouseenter(function(){
+    Audio13.play();
+});
+
+var Audio14= $("#Bulgaria_Audio")[0];
+$("#Bulgaria")
+.mouseenter(function(){
+    Audio14.play();
+});
+
+var Audio15= $("#Macedonia_Audio")[0];
+$("#Macedonia")
+.mouseenter(function(){
+    Audio15.play();
+});
+
+var Audio16= $("#Albania_Audio")[0];
+$("#Albania")
+.mouseenter(function(){
+    Audio16.play();
+});
+
+var Audio17= $("#Kosovo_Audio")[0];
+$("#Kosovo")
+.mouseenter(function(){
+    Audio17.play();
+});
+
+var Audio18= $("#Montenegro_Audio")[0];
+$("#Montenegro")
+.mouseenter(function(){
+    Audio18.play();
+});
+
+var Audio19= $("#Serbia_Audio")[0];
+$("#Serbia")
+.mouseenter(function(){
+    Audio19.play();
+});
+
+var Audio20= $("#BosHer_Audio")[0];
+$("#BosHer")
+.mouseenter(function(){
+    Audio20.play();
+});
+
+var Audio21= $("#Croatia_Audio")[0];
+$("#Croatia")
+.mouseenter(function(){
+    Audio21.play();
+});
+
+var Audio22= $("#Slovenia_Audio")[0];
+$("#Slovenia")
+.mouseenter(function(){
+    Audio22.play();
+});
+
+var Audio23= $("#Hungary_Audio")[0];
+$("#Hungary")
+.mouseenter(function(){
+    Audio23.play();
+});
+
+var Audio24= $("#Slovakia_Audio")[0];
+$("#Slovakia")
+.mouseenter(function(){
+    Audio24.play();
+});
+
+var Audio25= $("#CzechRep_Audio")[0];
+$("#CzechRep")
+.mouseenter(function(){
+    Audio25.play();
+});
+
+var Audio26= $("#Poland_Audio")[0];
+$("#Poland")
+.mouseenter(function(){
+    Audio26.play();
+});
+
+var Audio27= $("#Austria_Audio")[0];
+$("#Austria")
+.mouseenter(function(){
+    Audio27.play();
+});
+
+var Audio28= $("#Italy_Audio")[0];
+$("#Italy")
+.mouseenter(function(){
+    Audio28.play();
+});
+
+var Audio29= $("#liechtenstein_Audio")[0];
+$("#Liechtenstein")
+.mouseenter(function(){
+    Audio29.play();
+});
+
+var Audio30= $("#Switzerland_Audio")[0];
+$("#Switzerland")
+.mouseenter(function(){
+    Audio30.play();
+});
+
+var Audio31= $("#Germany_Audio")[0];
+$("#Germany")
+.mouseenter(function(){
+    Audio31.play();
+});
+
+var Audio32= $("#Denmark_Audio")[0];
+$("#Denmark")
+.mouseenter(function(){
+    Audio32.play();
+});
+
+var Audio33= $("#Luxembourg_Audio")[0];
+$("#Luxembourg")
+.mouseenter(function(){
+    Audio33.play();
+});
+
+var Audio34= $("#Belgium_Audio")[0];
+$("#Belgium")
+.mouseenter(function(){
+    Audio34.play();
+});
+
+var Audio35= $("#Netherlands_Audio")[0];
+$("#Netherlands")
+.mouseenter(function(){
+    Audio35.play();
+});
+
+var Audio36= $("#Portugal_Audio")[0];
+$("#Portugal")
+.mouseenter(function(){
+    Audio36.play();
+});
+
+var Audio37= $("#Andorra_Audio")[0];
+$("#Andorra")
+.mouseenter(function(){
+    Audio37.play();
+});
+
+var Audio38= $("#Spain_Audio")[0];
+$("#Spain")
+.mouseenter(function(){
+    Audio38.play();
+});
+
+var Audio39= $("#France_Audio")[0];
+$("#France")
+.mouseenter(function(){
+    Audio39.play();
+});
+
+var Audio40= $("#Ireland_Audio")[0];
+$("#Ireland")
+.mouseenter(function(){
+    Audio40.play();
+});
+
+var Audio41= $("#UK_Audio")[0];
+$("#UK")
+.mouseenter(function(){
+    Audio41.play();
+});
+
+var Audio42= $("#Iceland_Audio")[0];
+$("#Iceland")
+.mouseenter(function(){
+    Audio42.play();
+});
